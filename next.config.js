8b20482094db79b9a528fd1083a5275650af9023
aftabparvez <20273871+aftabparvez@users.noreply.github.com>
2023-12-19 16:11:10 +0530

    testing-preview-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled:
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" &&
+    process.env.ANALYZE === "true",
+});
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
